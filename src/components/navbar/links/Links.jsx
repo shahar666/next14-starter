398c5a3bab908b501d420503a9624bf05c9cf282
Shahar Yonatiev <ajrvnkl123654@gmail.com>
2024-06-30 15:33:14 +0300

    basic design done. moving on to basic functionality.

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/navLink";
+import Image from "next/image";
 
 const links = [
     {
@@ -47,7 +48,14 @@ const Links = () => {
                 )
                 }
             </div>
-            <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}>Menu</button>
+            <Image
+                className={styles.menuButton}
+                src="/menu.png"
+                alt=""
+                width={30}
+                height={30}
+                onClick={() => setOpen((prev) => !prev)}
+            />
             {
                 open && <div className={styles.mobileLinks}>
                     {links.map((link => (
